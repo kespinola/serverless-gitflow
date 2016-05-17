e46ec7adc1336d85433f7c1b5f3e1394b47fd8c8
Kyle Espinola <ksespinola@gmail.com>
2016-05-16 22:12:23 -0700

    compare develop to master

--- functions/candidate/handler.js
+++ functions/candidate/handler.js
@@ -1,6 +1,3 @@
-export default (event, context) => {
-  // must return a promise, a JSON.stringify compatible data, null or nothing.
-  return {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  };
-};
+import candidate from './../../lib/services/candidate';
+
+export default ({ release }) => candidate(release);

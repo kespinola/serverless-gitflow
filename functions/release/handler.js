40d454e6e3c81db584eea2b6fe50404c40ee7966
Kyle Espinola <ksespinola@gmail.com>
2016-05-25 18:54:05 -0700

    create release using a push event. remove title from candidate body.

--- functions/release/handler.js
+++ functions/release/handler.js
@@ -1,7 +1,3 @@
-export default (event, context) => {
-  // must return a promise, a JSON.stringify compatible data, null or nothing.
-  return {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  }
-}
+import release from './../../lib/services/release';
 
+export default ({ repository }) => release(repository);

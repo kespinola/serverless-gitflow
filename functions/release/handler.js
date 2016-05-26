d0fc14133ffa85b9fe2b666b43b823fe99f153d6
Kyle Espinola <ksespinola@gmail.com>
2016-05-25 19:16:27 -0700

    check to make sure a master merge commit

--- functions/release/handler.js
+++ functions/release/handler.js
@@ -1,3 +1,17 @@
 import release from './../../lib/services/release';
+import { ifElse, not, isNil, equals, compose, lensProp, view } from 'ramda';
 
-export default ({ repository }) => release(repository);
+const { MASTER_BRANCH } = process.env;
+
+const i18n = {
+  notCandidateMerge: 'Not a closed master pull request',
+};
+
+export default compose(
+  ifElse(
+    ({ merged_at, head: { ref } }) => not(isNil(merged_at)) && equals(ref, MASTER_BRANCH),
+    release,
+    () => ({ message: i18n.notCandidateMerge, status: 422 })
+  ),
+  view(lensProp('repository'))
+);
